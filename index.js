b4f2201b3498336b24b45696b1e7dcfd5ee9dfca
Fabricio Dourado <fcvieira2@gmail.com>
2022-04-11 21:39:21 -0300

    Propriedades privadas

--- index.js
+++ index.js
@@ -8,27 +8,34 @@ class Cliente {
 class ContaCorrente {
     constructor (agencia, saldo) {
         this.agencia = agencia
-        this.saldo = saldo
+        this._saldo = saldo
     }
 
     sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor
+        if (this._saldo >= valor) {
+            this._saldo -= valor
         }
     }
 
     depositar (valor) {
-        this.saldo = valor
+        if (valor >= 0) {
+            this._saldo = valor
+        }
+    }
+
+    extrato () {
+        console.log(`Saldo é ${this._saldo}`)
     }
 }
 
 const cliente_1 = new Cliente('Fabrício','111-111-111-11')
 const cliente_2 = new Cliente('Natalia','111-111-111-11')
 
-const contaCorrenteFabricio = new ContaCorrente('0137', '0,00')
+const contaCorrenteFabricio = new ContaCorrente('0137', 0)
 contaCorrenteFabricio.depositar(1000)
-contaCorrenteFabricio.sacar(1100)
+contaCorrenteFabricio.sacar(200)
+contaCorrenteFabricio.extrato()
 
-console.log(cliente_1, cliente_2)
-console.log(contaCorrenteFabricio)
+// console.log(cliente_1, cliente_2)
+// console.log(contaCorrenteFabricio)
 
