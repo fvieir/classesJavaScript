8050583ba436be9410c8a1c8b2a96fea87d79927
Fabricio Dourado <fcvieira2@gmail.com>
2022-04-13 06:13:06 -0300

    Adicionando acessores de atributos privados

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,8 +1,19 @@
+import { Cliente } from "./Cliente.js"
+
 export class ContaCorrente {
     constructor (agencia, saldo, cliente) {
         this.agencia = agencia
-        this.cliente = cliente
+        this._cliente = cliente
         this._saldo = saldo
+    }
+
+    set cliente (novoValor) {
+        if (novoValor instanceof Cliente)
+        this._cliente = novoValor
+    }
+
+    get cliente () {
+        return this._cliente
     }
 
     sacar(valor) {
--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const cliente_2 = new Cliente('Natalia','111-111-111-11')
 const contaCorrenteFabricio = new ContaCorrente('0137', 0)
 contaCorrenteFabricio.depositar(1000)
 contaCorrenteFabricio.sacar(200)
-// contaCorrenteFabricio.extrato()
+contaCorrenteFabricio.extrato()
 contaCorrenteFabricio.cliente = cliente_1
 
 const conta_2 = new ContaCorrente('2587',0)
 contaCorrenteFabricio.transferir(100,conta_2)
 conta_2.cliente = cliente_2
 
-console.log(conta_2)
-console.log(cliente_2)
+console.log(conta_2.cliente)
