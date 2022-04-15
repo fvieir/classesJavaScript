7b4ed077ddbf1e4d6673820eea358d1e34cee9f8
Fabricio Dourado <fcvieira2@gmail.com>
2022-04-15 10:36:25 -0300

    Adicionando atributos estaticos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,7 @@
 import { Cliente } from "./Cliente.js"
 
 export class ContaCorrente {
-
+    static numeroDeContas = 0;
     agencia;
     _cliente;
     _saldo = 0;
@@ -9,6 +9,7 @@ export class ContaCorrente {
     constructor (agencia, cliente) {
         this.agencia = agencia
         this.cliente = cliente
+        ContaCorrente.numeroDeContas += 1
     }
 
     set cliente (novoCliente) {
--- index.js
+++ index.js
@@ -7,11 +7,4 @@ const cliente_2 = new Cliente('Natalia','111-111-111-11')
 const contaCorrenteFabricio = new ContaCorrente('0137', cliente_1)
 const conta_2 = new ContaCorrente('2587',cliente_2)
 
-contaCorrenteFabricio.depositar(1000)
-contaCorrenteFabricio.sacar(200)
-contaCorrenteFabricio.extrato()
-
-contaCorrenteFabricio.transferir(100,conta_2)
-
-console.log(contaCorrenteFabricio)
-console.log(conta_2)
+console.log(ContaCorrente.numeroDeContas)
